8bf0deb1512183814605aa55d2bdf48845201713
Duc Toan <toannd2409@gmail.com>
2019-04-04 15:41:24 +0700

    Initial commit

---
+++ app/src/main/java/com/ndt/toane/demoretrofit/JsonPlaceHolderApi.java
@@ -0,0 +1,11 @@
+package com.ndt.toane.demoretrofit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderApi {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
